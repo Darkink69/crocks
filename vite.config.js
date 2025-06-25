d77af2b13288e2b8ef6cf2553e7de3d0cb6e4e79
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-06-25 17:23:22 +0700

    vite conf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/crocks/',
+    base: '/',
     css: {
         preprocessorOptions: {
             scss: {
